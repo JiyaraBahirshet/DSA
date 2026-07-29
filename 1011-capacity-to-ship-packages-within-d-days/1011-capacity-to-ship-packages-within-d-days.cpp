2968e482a0d539a6645493484bc680be6580f919
Jiyara Bahirshet <jiyarabahirshet11@gmail.com>
2026-07-29 23:06:16 +0530

    Time: 13 ms (40.45%), Space: 34.9 MB (88.95%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    int findDays(vector<int>& weights, int capacity) {
+
+        int days = 1;
+        int load = 0;
+
+        for (int weight : weights) {
+
+            if (load + weight <= capacity)
+                load += weight;
+            else {
+                days++;
+                load = weight;
+            }
+        }
+
+        return days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+
+        int low = *max_element(weights.begin(), weights.end());
+
+        int high = accumulate(weights.begin(), weights.end(), 0);
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int requiredDays = findDays(weights, mid);
+
+            if (requiredDays <= days)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+};
