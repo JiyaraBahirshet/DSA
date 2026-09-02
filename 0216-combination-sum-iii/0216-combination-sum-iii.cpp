2ab4ea56bcc4e8d27cf9fb6bf22b7da92f2ea701
Jiyara Bahirshet <jiyarabahirshet11@gmail.com>
2026-09-02 20:07:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (97.47%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    void findcombination(int start,int k,int target,vector<int>&ds,vector<vector<int>> & ans ){
+        if(k==0){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return ;
+        }
+        for(int i=start;i<=9;i++){
+            if(i>target){
+                break;
+            }
+            ds.push_back(i);
+            findcombination(i+1,k-1,target-i,ds,ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findcombination(1,k,n,ds,ans);
+        return ans;
+    }
+};
