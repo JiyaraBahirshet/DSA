0d503cc4e4e3a303955839ef0976bdfcf835be5c
Jiyara Bahirshet <jiyarabahirshet11@gmail.com>
2026-09-10 09:50:00 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.56%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
-       unordered_map<int,int>mp;
-       for(int num:nums){
-        mp[num]++;
-       }
-       for(auto it:mp){
-        if(it.second==1){
-            return it.first;
+        int ans=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            ans=ans^nums[i];
         }
-       }
-       return -1;
+        return ans;
     }
 };
