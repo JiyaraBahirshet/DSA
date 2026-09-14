cbfcd0b6584a55cad5b9149a1258dfba7361ae1b
Jiyara Bahirshet <jiyarabahirshet11@gmail.com>
2026-09-14 09:45:24 +0530

    Time: 0 ms (100%), Space: 10 MB (88.79%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,33 @@
+class MyStack {
+public:
+    queue<int> q;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q.push(x);
+
+        int s = q.size();
+
+        for(int i = 0; i < s - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
